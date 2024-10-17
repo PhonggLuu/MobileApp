372b66174a8aa5b0a2340a40c3b89d9503f8595a
ontrip <hoangtoc202@gmail.com>
2024-10-17 13:30:51 +0700

    Remove Application

--- Application/app/src/main/java/com/application/view/LoginView.java
+++
@@ -1,9 +0,0 @@
-package com.application.view;
-
-public interface LoginView {
-    void showEmptyFieldsError();
-    void showLoginSuccess();
-    void showLoginFailed();
-    void showLoginError(String s);
-    void navigateToWelcome();
-}
