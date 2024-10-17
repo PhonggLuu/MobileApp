fd54d204d8caf13ebcb002c925b2d34016583afe
ontrip <hoangtoc202@gmail.com>
2024-10-17 13:28:02 +0700

    Project reinitialized

---
+++ DemoSql/app/src/main/java/com/demosql/network/ApiClient.java
@@ -0,0 +1,40 @@
+package com.demosql.network;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private static final String BASE_URL = "https://api.hamazaki.online/api/";
+    private static ApiClient instance;
+    private ApiService apiService;
+
+    private ApiClient() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(loggingInterceptor)
+                .build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        apiService = retrofit.create(ApiService.class);
+    }
+
+    public static synchronized ApiClient getInstance() {
+        if (instance == null) {
+            instance = new ApiClient();
+        }
+        return instance;
+    }
+
+    public ApiService getApiService() {
+        return apiService;
+    }
+}
---
+++ DemoSql/app/src/main/java/com/demosql/view/LoginActivity.java
@@ -0,0 +1,81 @@
+package com.demosql.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.demosql.databinding.SigninLayoutBinding;
+import com.demosql.presenter.LoginPresenter;
+
+public class LoginActivity extends AppCompatActivity implements LoginView {
+    private SigninLayoutBinding binding;
+    private LoginPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = SigninLayoutBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        presenter = new LoginPresenter(this);
+        binding.login.setOnClickListener(view ->
+        {
+            String email = binding.logUsername.getText().toString().trim();
+            String password = binding.logPassword.getText().toString().trim();
+            try {
+                presenter.handleLogin(email, password);
+            } catch (Exception e) {
+                Log.e("LoginActivity", "Error during login", e);
+                Toast.makeText(this, "Có lỗi xảy ra. Vui lòng thử lại!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.signup.setOnClickListener(view ->
+        {
+            Intent intent = new Intent(this, SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.dangnhap, (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    @Override
+    public void navigateToWelcome() {
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        startActivity(intent);
+        finish();  // Optional: finish the LoginActivity if you don’t want to return to it
+    }
+
+    @Override
+    public void showEmptyFieldsError() {
+        Toast.makeText(this, "Email và mật khẩu không được để trống", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showLoginSuccess() {
+        Toast.makeText(this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, WelcomeActivity.class);
+    }
+
+    @Override
+    public void showLoginFailed() {
+        Toast.makeText(this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void showLoginError(String s) {
+        Toast.makeText(this, "Đăng nhập thất bại", Toast.LENGTH_SHORT).show();
+    }
+}
