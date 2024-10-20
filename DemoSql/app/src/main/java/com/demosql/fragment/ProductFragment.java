83308e96c1dc6bf666b8c0f8ed156683c223e424
ontrip <hoangtoc202@gmail.com>
2024-10-20 21:47:40 +0700

    Layout(Products): Update new layout.

--- DemoSql/app/src/main/java/com/demosql/fragment/ProductFragment.java
+++ DemoSql/app/src/main/java/com/demosql/fragment/ProductFragment.java
@@ -5,9 +5,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.ProgressBar;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -37,7 +39,7 @@ public class ProductFragment extends Fragment implements ProductView {
         presenter.loadProducts();
         // Initialize RecyclerView
         recyclerView = rootView.findViewById(R.id.productRecyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext())); // Set LayoutManager
+        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2)); // Set LayoutManager
 
         return rootView;
     }
